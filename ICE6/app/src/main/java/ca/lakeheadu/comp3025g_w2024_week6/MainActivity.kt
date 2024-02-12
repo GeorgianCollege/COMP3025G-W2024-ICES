b2e2decf13cf5e285419ece8f4449254d055418b
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-02-12 11:44:04 -0500

    added functions to read from assets or res/raw folders

--- ICE6/app/src/main/java/ca/lakeheadu/comp3025g_w2024_week6/MainActivity.kt
+++ ICE6/app/src/main/java/ca/lakeheadu/comp3025g_w2024_week6/MainActivity.kt
@@ -1,5 +1,6 @@
 package ca.lakeheadu.comp3025g_w2024_week6
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,9 +12,22 @@ class MainActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var contact = ContactModel("Tom Tsiliopoulos", "4165555555", "tom@example.com")
+        Log.i("onResource", getTextFromResource(this, R.raw.contacts));
+        Log.i("onAsset", getTextFromAsset(this, "contacts.json"))
 
-        Log.i("onCreate", contact.toString())
-        println(contact.toString())
+    }
+
+    private fun getTextFromResource(context: Context, resourceId: Int): String
+    {
+        return context.resources.openRawResource(resourceId)
+            .bufferedReader()
+            .use { it.readText()}
+    }
+
+    private fun getTextFromAsset(context: Context, fileName: String): String
+    {
+        return context.resources.assets.open(fileName)
+            .bufferedReader()
+            .use { it.readText()}
     }
 }
