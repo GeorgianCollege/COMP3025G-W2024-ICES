b2b83bf3f5c1a234373ae55ef4662d0c8dbc26aa
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-03-18 09:50:15 -0400

    added Project files for ICE10

---
+++ ICE9/app/src/main/java/ca/lakeheadu/comp3025g_w2024_week10/FirebaseMovie.kt
@@ -0,0 +1,6 @@
+package ca.lakeheadu.comp3025g_w2024_week10
+
+
+data class FirebaseMovie(
+    val title: String = "",
+    val studio: String = "")
---
+++ ICE9/app/src/main/java/ca/lakeheadu/comp3025g_w2024_week10/FirestoreDataManager.kt
@@ -0,0 +1,28 @@
+package ca.lakeheadu.comp3025g_w2024_week10
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObject
+
+class FirestoreDataManager
+{
+    private val db = FirebaseFirestore.getInstance()
+    private val collectionRef = db.collection("movies")
+
+    fun getMovies(onComplete: (List<FirebaseMovie>) -> Unit)
+    {
+        collectionRef.get()
+            .addOnSuccessListener { result->
+                val movies = result.mapNotNull { it.toObject<FirebaseMovie>() }
+                onComplete(movies)
+            }
+            .addOnFailureListener{
+                onComplete(emptyList())
+            }
+    }
+
+    fun addMovie(movie: FirebaseMovie, onComplete: (Boolean)-> Unit) {
+     collectionRef.add(movie)
+         .addOnSuccessListener { onComplete(true) }
+         .addOnFailureListener { onComplete(false) }
+    }
+}
